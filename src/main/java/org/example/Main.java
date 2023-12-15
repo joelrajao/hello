f007f0d947e49aeca7ff83f0326d218651923428
joelrajao <j.rajaomilison@gmail.com>
2023-12-15 08:24:05 +0100

    Changement de code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Bonjour world!");
+		System.out.println("Bonjour world! Je teste mon changement de code");
 	}
 }
