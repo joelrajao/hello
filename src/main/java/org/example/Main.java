afb8e7c4e84eb456e60b2ea03fb2061ad8760d67
joelrajao <41187491+joelrajao@users.noreply.github.com>
2023-12-16 07:10:01 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Bonjour world! Je teste mon changement de code");
+		System.out.println("Bonjour world! Je teste mon changement de code pour le trigger cette fois-ci");
 	}
 }
